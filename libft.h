2482d42862fdce5bf26142dafffeb89d040c7e8e
iury <iuryroberto@gmail.com>
2023-08-18 16:20:35 -0300

    (fix) changed GNL buffer size from 1 to 4096 bytes

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: iusantos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 09:10:21 by iusantos          #+#    #+#             */
-/*   Updated: 2023/08/18 15:48:43 by iusantos         ###   ########.fr       */
+/*   Updated: 2023/08/18 16:20:09 by iusantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,12 +66,12 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
-int	count_word(const char *s, char c);
+int	count_word(const char *s, char c); // organizar isso aqui depois
 
 /**** get_next_line ****/
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1
+#  define BUFFER_SIZE 4096
 # endif //BUFFER_SIZE
 
 char	*get_next_line(int fd);
