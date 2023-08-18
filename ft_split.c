088faaa57a0713b895d3b5eff4e0a908b1802dcf
iury <iuryroberto@gmail.com>
2023-08-18 15:51:52 -0300

    (fix) unerased word_dup function in ft_split

--- ft_split.c
+++ ft_split.c
@@ -6,11 +6,29 @@
 /*   By: iusantos <iusantos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 15:02:40 by iusantos          #+#    #+#             */
-/*   Updated: 2023/08/18 15:49:11 by iusantos         ###   ########.fr       */
+/*   Updated: 2023/08/18 15:51:05 by iusantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static char	*word_dup(const char *str, size_t start, size_t end)
+{
+	char	*word;
+	size_t	i;
+
+	word = malloc((end - start + 1) * sizeof(char));
+	if (!word)
+		return (0);
+	i = 0;
+	while (i < end - start)
+	{
+		word[i] = str[start + i];
+		i++;
+	}
+	word[i] = '\0';
+	return (word);
+}
 
 char	**ft_split(char const *s, char c)
 {
