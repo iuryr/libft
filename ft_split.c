afce577faaa4202262a15b33ffc6850b2ec3318a
iury <iuryroberto@gmail.com>
2023-09-20 19:30:52 -0300

    pra continuar de casa

--- ft_split.c
+++ ft_split.c
@@ -6,54 +6,35 @@
 /*   By: iusantos <iusantos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 15:02:40 by iusantos          #+#    #+#             */
-/*   Updated: 2023/08/18 15:51:05 by iusantos         ###   ########.fr       */
+/*   Updated: 2023/09/20 19:10:01 by iusantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	*word_dup(const char *str, size_t start, size_t end)
-{
-	char	*word;
-	size_t	i;
-
-	word = malloc((end - start + 1) * sizeof(char));
-	if (!word)
-		return (0);
-	i = 0;
-	while (i < end - start)
-	{
-		word[i] = str[start + i];
-		i++;
-	}
-	word[i] = '\0';
-	return (word);
-}
-
 char	**ft_split(char const *s, char c)
 {
-	size_t	i;
-	size_t	j;
-	int		word_start;
 	char	**split;
+	char	**return_split;
+	int		nbr_words;
+	int		i;
+	int		size;
 
-	split = ft_calloc((count_word(s, c) + 1), sizeof(char *));
+	nbr_words = count_word(s, c);
+	split = ft_calloc(nbr_words + 1, sizeof (char *));
+	return_split = split;
 	if (split == NULL)
-		return (0);
+		return (NULL);
 	if (!*s)
 		return (split);
 	i = -1;
-	j = 0;
-	word_start = -1;
-	while (++i < ft_strlen(s) + 1)
+	while (++i < nbr_words)
 	{
-		if (s[i] != c && word_start < 0)
-			word_start = i;
-		else if ((s[i] == c || i == ft_strlen(s)) && word_start >= 0)
-		{
-			split[j++] = word_dup(s, word_start, i);
-			word_start = -1;
-		}
+		size = ft_strchr(s, c) - s;
+		*split++ = ft_substr(s, 0, size);
+		s += size;
+		while (*s == c)
+			s++;
 	}
-	return (split);
+	return (return_split);
 }
